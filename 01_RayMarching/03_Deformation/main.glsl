ffb62846b646f4ccd0799cc0bfbc607aa172c267
Moto0116 <moto01160902@gmail.com>
2019-12-09 23:19:39 +0900

    03_Deformation更新 全体的なreadme修正

--- 01_RayMarching/03_Deformation/main.glsl
+++ 01_RayMarching/03_Deformation/main.glsl
@@ -1,7 +1,7 @@
 
 #define MAX_VALUE 1e10
 #define EPSILON 0.00001
-#define MARCHING_LOOP 128
+#define MARCHING_LOOP 256
 
 // 球体の距離関数
 float dfSphere(vec3 p, float r) {
@@ -11,7 +11,6 @@ float dfSphere(vec3 p, float r) {
 // ボックスの距離関数
 float dfBox(vec3 p, vec3 b)
 {
-
     vec3 q = abs(p) - b;
     return 
         length(max(q, 0.0)) + 
@@ -27,16 +26,28 @@ float dfCompare(float a, float b) {
 float map(vec3 p) {
     float result = MAX_VALUE;
 
-    // vec3 bp = p + vec3(-2.5, 0, 0);
-    // const float k = 2.0;
-    // float c = cos(k * bp.y);
-    // float s = sin(k * bp.y);
-    // mat2  m = mat2(c, -s, s, c);
-    // vec3  q = vec3(m * bp.xz, bp.y);
-    // result = dfCompare(result, dfBox(q, vec3(1, 1, 2)));
+    // // でこぼこ球体表示
+    // vec3 sp = p + vec3(-12., 0, 0);
+    // const float n = 0.85;
+    // float d1 = dfSphere(sp, 10.);
+    // float d2 = sin(sp.x * n) * sin(sp.y * n) * sin(sp.z * n);
+    // result = dfCompare(result, (d1 + d2));
+    // // 通常の球体表示
+    // sp = p + vec3(12., 0, 0);
+    // result = dfCompare(result, dfSphere(sp, 10.));
 
-    // bp = p + vec3(2.5, 0, 0);
-    // result = dfCompare(result, dfBox(bp, vec3(1, 2, 1)));
+    // 変形したボックス表示
+    vec3 bp = p + vec3(-10., 0, 0);
+    const float k = 0.6;
+    float c = cos(k * bp.y);
+    float s = sin(k * bp.y);
+    mat2  m = mat2(c, -s, s, c);
+    vec2 xz = m * bp.xz;
+    vec3  q = vec3(xz.x, bp.y, xz.y);
+    result = dfCompare(result, dfBox(q, vec3(3, 15, 1)) - .3);
+    // 通常のボックス表示
+    bp = p + vec3(10., 0, 0);
+    result = dfCompare(result, dfBox(bp, vec3(3, 15, 1)) - .3);
 
     return result;
 }
@@ -54,7 +65,7 @@ vec3 mapNormal(vec3 p) {
 void mainImage(out vec4 fragColor, in vec2 fragCoord) {
     vec2 pixelPos = (fragCoord.xy * 2. - iResolution.xy) / min(iResolution.x, iResolution.y); // 原点を画面中心に
 
-    vec3 cameraPos  = vec3( 0.0,  0.0,  5.0);   // カメラ座標
+    vec3 cameraPos  = vec3( 0.0,  0.0, 30.0);   // カメラ座標
     vec3 cameraDir  = vec3( 0.0,  0.0, -1.0);   // カメラ向き
     vec3 cameraUp   = vec3( 0.0,  1.0,  0.0);   // カメラ上方向ベクトル
     vec3 cameraSide = cross(cameraDir, cameraUp); // 横方向ベクトル
